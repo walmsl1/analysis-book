8041a58562de0d144cc98b8685ba5778639112a1
David Walmsley <walmsl1@stolaf.edu>
2025-02-07 15:46:53 -0600

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -25,7 +25,7 @@ var ptx_lunr_docs = [
   "type": "Acknowledgements",
   "number": "",
   "title": "Acknowledgements",
-  "body": "We are grateful to Layton Borst, whose work througout Summer 2024 got this book up and running. "
+  "body": " We are grateful to Layton Borst, whose work througout Summer 2024 got this book up and running.  "
 },
 {
   "id": "xOf",
